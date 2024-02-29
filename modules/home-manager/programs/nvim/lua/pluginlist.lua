011c7a87f8e60fcf8ed19cbe007681881970fc45
Jude Shreffler <js467820@ohio.edu>
2024-02-29 18:06:36 -0500

    Added cmp + dependencies to pluginlist, defined completion in after/plugin/cmp.lua, and required cmp capabilities in lsp.lua

--- modules/home-manager/programs/nvim/lua/pluginlist.lua
+++ modules/home-manager/programs/nvim/lua/pluginlist.lua
@@ -35,5 +35,18 @@ return {
         theme = "nord"
       })
     end,
-  }
+  },
+
+  -- code completion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      
+      "hrsh7th/cmp-nvim-lsp",
+      
+    },
+  },
 }
