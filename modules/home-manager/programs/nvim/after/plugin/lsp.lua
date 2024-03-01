1bc6ee584b97f8e6d65168f139f8d57029e48b9f
Jude Shreffler <js467820@ohio.edu>
2024-02-29 19:00:45 -0500

    added on_attach and capabilities to lua_ls

--- modules/home-manager/programs/nvim/after/plugin/lsp.lua
+++ modules/home-manager/programs/nvim/after/plugin/lsp.lua
@@ -30,7 +30,10 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 require("neodev").setup()
-require("lspconfig").lua_ls.setup {}
+require("lspconfig").lua_ls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
 
 --[[
 require("mason").setup()
