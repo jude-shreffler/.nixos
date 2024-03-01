cf86f30edd4ff0f668bf632647e7feb64aec4b9a
Jude Shreffler <js467820@ohio.edu>
2024-02-29 19:08:17 -0500

    Added telescope to pluginlist.lua

--- modules/home-manager/programs/nvim/lua/pluginlist.lua
+++ modules/home-manager/programs/nvim/lua/pluginlist.lua
@@ -59,4 +59,15 @@ return {
       require("carbon").setup()
     end
   },
+
+  -- telescope
+  { 
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' } 
+  },
+
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+  }, 
 }
