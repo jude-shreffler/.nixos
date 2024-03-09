73e9eb39614d4ab709792bf79dfa4a332954bb5a
Jude Shreffler <js467820@ohio.edu>
2024-03-09 17:36:04 -0500

    Changed fish's config to use leftwm rebuild + screenshot scripts

--- modules/home-manager/programs/fish/config.fish
+++ modules/home-manager/programs/fish/config.fish
@@ -1,33 +1,10 @@
-if status is-interactive
-  pfetch
-end
-
 set -x EDITOR nvim
 set -x SHELL fish
 set -x XDG_CONFIG_HOME /home/js/.config
 
-function rebuild
-  cd /home/js/.nixos
-  git add *
-  git commit
-  sudo nixos-rebuild switch --flake ~/.nixos#(hostname)
-end
+alias rebuild="~/.config/leftwm/rebuild.sh"
+alias screenshot="~/.config/leftwm/screenshot.sh"
 
-function screenshot
-  set year (date +"%Y")
-  set month (date +"%b")
-  set day (date +"%d")
-  set time (date +"%H:%M:%S")
-
-  if not test -e ~/Pictures/Screenshots/$year
-    mkdir ~/Pictures/Screenshots/$year
-  end
-  if not test -e ~/Pictures/Screenshots/$year/$month
-    mkdir ~/Pictures/Screenshots/$year/$month
-  end
-  if not test -e ~/Pictures/Screenshots/$year/$month/$day
-    mkdir ~/Pictures/Screenshots/$year/$month/$day
-  end
-
-  scrot -F ~/Pictures/Screenshots/$year/$month/$day/$time.png
+if status is-interactive
+  pfetch
 end
