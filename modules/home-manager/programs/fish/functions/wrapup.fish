9f6d8223d3966fb37cb44375a9db49be44c7fd0d
Jude Shreffler <js467820@ohio.edu>
2024-03-10 17:36:13 -0400

    added some debugging to wrapup.fish

--- modules/home-manager/programs/fish/functions/wrapup.fish
+++ modules/home-manager/programs/fish/functions/wrapup.fish
@@ -8,18 +8,23 @@ set -g id $argv[3]
 echo "Wrapup debug:\nName: $name\nOutput: $output\nID: $id\n"
 set -g run "true"
 while test $run = "true"
-  sleep 30
+  echo "Sleeping..."
+  sleep 10 &
+  wait $last_pid
   if test (transmission-remote -l | grep $name | string sub -s 27 -l 4) = "Done"
     set -g run "false"
+    echo "Transmission Done!"
   end
 end
 
 chmod 644 /home/js/torrents/$name/*.mp4
 chown jellyfin:jellyfin /home/js/torrents/$name/*.mp4
 mv /home/js/torrents/$name/*.mp4 /home/js/movies/$output
+echo "Moved to movies"
 
 if transmission-remote -t $id -rad | grep success > /dev/null
 else
   echo "Error removing torrent. Exiting."
   exit 2
 end
+echo "Removed torrent and directory"
