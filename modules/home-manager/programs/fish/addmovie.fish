fc2aba512cbbc07eda0ca74ea4e39d48445ac626
Jude Shreffler <js467820@ohio.edu>
2024-03-10 17:14:25 -0400

    Changed the scope of some variables

--- modules/home-manager/programs/fish/addmovie.fish
+++ modules/home-manager/programs/fish/addmovie.fish
@@ -2,13 +2,13 @@
 
 if test (count $argv) -eq 2
   set -g input $argv[1]
-  set -x output $argv[2]
+  set -g output $argv[2]
   echo debug: run
-  if transmission-remote -a $argv[1] | grep success > /dev/null
-    set -g linenum (math (transmission-remote -l | count) - 1)
-    set -g info (transmission-remote -l | sed "$linenum!d")
-    set -x id (string sub -l 6 $info)
-    set -x name (string sub -s 73 $info)
+  if transmission-remote -a $input | grep success > /dev/null
+    set -l linenum (math (transmission-remote -l | count) - 1)
+    set -l info (transmission-remote -l | sed "$linenum!d")
+    set -l id (string sub -l 6 $info)
+    set -l name (string sub -s 73 $info)
     /home/js/.config/fish/wrapup.fish $name $output $id &
 
   else
