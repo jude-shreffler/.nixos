79b73417b8d990391df2bc623c3451d8d4d93edc
Jude Shreffler <js467820@ohio.edu>
2024-04-12 13:17:56 -0400

    Aliasing transmission-remote

--- modules/home-manager/programs/fish/config.fish
+++ modules/home-manager/programs/fish/config.fish
@@ -5,6 +5,7 @@ set -x XDG_CONFIG_HOME /home/js/.config
 alias rebuild="/home/js/.config/fish/functions/rebuild.fish"
 alias screenshot="/home/js/.config/fish/functions/screenshot.fish"
 alias addm="sudo /home/js/.config/fish/functions/addmovie.fish"
+alias tr="transmission-remote"
 
 if status is-interactive
   pfetch
