94ee3437cd53ac8148ec69157e8c6adbd6a554e2
Jude Shreffler <js467820@ohio.edu>
2024-04-21 17:41:20 -0400

    Added garbage collection command for bootloader generations

--- modules/home-manager/programs/fish/config.fish
+++ modules/home-manager/programs/fish/config.fish
@@ -3,6 +3,7 @@ set -x SHELL fish
 set -x XDG_CONFIG_HOME /home/js/.config
 
 alias rebuild="/home/js/.config/fish/functions/rebuild.fish"
+alias garbage="sudo nix-collect-garbage --delete-older-than 14d"
 alias screenshot="/home/js/.config/fish/functions/screenshot.fish"
 alias addm="sudo /home/js/.config/fish/functions/addmovie.fish"
 alias tr="transmission-remote"
